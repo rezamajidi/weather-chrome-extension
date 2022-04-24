b4b402f54804b47840545a5b890cb937eb9727a4
Reza <rezamajidi.1992@gmail.com>
2022-04-24 20:16:51 +0430

    refactor: popup card

--- src/popup/WeatherCard/WeatherCard.tsx
+++ src/popup/WeatherCard/WeatherCard.tsx
@@ -2,30 +2,51 @@ import React, { useEffect, useState } from 'react';
 import { Box, Card, CardContent, Typography } from '@material-ui/core'
 import { fetchOpenWeatherData, OpenWeatherData } from '../../utils/api'
 
+const WeatherCardContainer: React.FC<{
+  children: React.ReactNode
+}> = ({children}) => (
+  <Box mx={"4px"} my={"16px"}>
+    <Card>
+      <CardContent>{children}</CardContent>
+    </Card>
+  </Box>
+)
+
+type WeatherCardState = "loading" | "error" | "ready" | "done"
+
 const WeatherCard: React.FC<{
   city: string
 }> = ({ city }) => {
-  const [weatherData, setWeatherData] = useState<OpenWeatherData | null>(null) 
+  const [weatherData, setWeatherData] = useState<OpenWeatherData | null>(null)
+  const [cardState, setCardState] = useState<WeatherCardState>("loading")
   useEffect(() => {
     fetchOpenWeatherData(city)
       .then((data) => {
         setWeatherData(data)
+        setCardState("ready")
       })
-      .catch((err) => console.error(err))
+      .catch((err) => {
+        setCardState("error")
+      })
   },[city])
-  if(!weatherData) {
-    return <div>Loading...</div>
+  if(cardState === "loading" || cardState === "error") {
+    return(
+      <WeatherCardContainer>
+        <Typography variant="body1">
+          { cardState === "loading"
+            ? 'Loading...'
+            : `Could not retrieve weather data for the ${city} city`
+          }
+        </Typography>
+      </WeatherCardContainer>
+    )
   }
   return (
-    <Box mx={"4px"} my={"16px"}>
-      <Card>
-        <CardContent>
-          <Typography variant="h5">{weatherData.name}</Typography>
-          <Typography variant="body1">{Math.round(weatherData.main.temp)}</Typography>
-          <Typography variant="body1">Feels like:{Math.round(weatherData.main.feels_like)}</Typography>
-        </CardContent>
-      </Card>
-    </Box>
+    <WeatherCardContainer>
+      <Typography variant="h5">{weatherData.name}</Typography>
+      <Typography variant="body1">{Math.round(weatherData.main.temp)}</Typography>
+      <Typography variant="body1">Feels like:{Math.round(weatherData.main.feels_like)}</Typography>
+    </WeatherCardContainer>
   )
 }
 
