1eec28aca6b6a69d8899db6f7a7aae47006593d7
Reza <rezamajidi.1992@gmail.com>
2022-04-30 01:44:53 +0430

    feat: add icon image for weather

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Button, Card, CardContent, CardActions, Typography } from '@material-ui/core'
-import { fetchOpenWeatherData, OpenWeatherData, OpenWeatherTempScale } from '../../utils/api'
+import { Box, Button, Card, CardContent, CardActions, Grid, Typography } from '@material-ui/core'
+import { fetchOpenWeatherData, getWeatherIconSrc, OpenWeatherData, OpenWeatherTempScale } from '../../utils/api'
 import './WeatherCard.css'
 
 const WeatherCardContainer: React.FC<{
@@ -56,9 +56,23 @@ const WeatherCard: React.FC<{
   }
   return (
     <WeatherCardContainer onDelete={onDelete}>
-      <Typography className="weatherCard-title">{weatherData.name}</Typography>
-      <Typography className="weatherCard-body">{Math.round(weatherData.main.temp)}</Typography>
-      <Typography className="weatherCard-body">Feels like:{Math.round(weatherData.main.feels_like)}</Typography>
+      <Grid container justifyContent="space-around">
+        <Grid item>
+          <Typography className="weatherCard-title">{weatherData.name}</Typography>
+          <Typography className="weatherCard-temp">{Math.round(weatherData.main.temp)}</Typography>
+          <Typography className="weatherCard-body">Feels like:{Math.round(weatherData.main.feels_like)}</Typography>
+        </Grid>
+        <Grid item>
+          {weatherData.weather.length > 0 && (
+              <>
+                <img src={getWeatherIconSrc(weatherData.weather[0].icon)} />
+                <Typography className="weatherCard-body">
+                  {weatherData.weather[0].main}
+                </Typography>
+              </>
+          )}
+        </Grid>
+      </Grid>
     </WeatherCardContainer>
   )
 }
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -37,3 +37,7 @@ export async function fetchOpenWeatherData(
   const data: OpenWeatherData = await res.json()
   return data
 }
+
+export function getWeatherIconSrc(iconCode: string): string {
+  return `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+}
