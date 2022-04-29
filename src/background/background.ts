a4cb4f79b3597dc55b322978685b5637f758dd18
Reza <rezamajidi.1992@gmail.com>
2022-04-30 00:59:55 +0430

    feat: add badge for home city temp

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,9 +1,40 @@
-import { setStoredCities, setStoredOptions } from '../utils/storage'
+import { fetchOpenWeatherData } from '../utils/api'
+import { setStoredCities, getStoredCities, setStoredOptions, getStoredOptions  } from '../utils/storage'
 chrome.runtime.onInstalled.addListener(() => {
   setStoredCities([])
   setStoredOptions({
     hasAutoOverlay: false,
     homeCity: '',
     tempScale: 'metric'
+  })
+
+  chrome.contextMenus.create({
+    contexts: ['selection'],
+    title: 'Add this city to weather extension',
+    id: 'weatherExtension'
+  })
+
+  chrome.alarms.create({
+    periodInMinutes: 60,
+  })
+})
+
+chrome.contextMenus.onClicked.addListener((event) => {
+  getStoredCities().then((cities) => {
+    setStoredCities([...cities, event.selectionText])
+  })
+})
+chrome.alarms.onAlarm.addListener(()=> {
+  getStoredOptions().then((options) => {
+    if(options.homeCity === '') {
+      return
+    }
+    fetchOpenWeatherData(options.homeCity,options.tempScale).then((data) => {
+      const temp = Math.round(data.main.temp)
+      const symbol = options.tempScale === 'metric' ? '\u2103' : '\u2109'
+      chrome.action.setBadgeText({
+        text: `${temp}${symbol}`
+      })
+    })
   })
 })
