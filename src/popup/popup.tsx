b67c20a83551e60a6728d1a0d642b7ee52ec4267
Reza <rezamajidi.1992@gmail.com>
2022-04-30 04:33:00 +0430

    style: refactor popup topbar

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { Box, Grid, InputBase, IconButton, Paper, Icon } from '@material-ui/core'
+import { Box, Button, ButtonGroup, Grid, InputAdornment, IconButton, TextField } from '@material-ui/core'
 import { Add as AddIcon, PictureInPicture as PictureInPictureIcon } from '@material-ui/icons'
 import 'fontsource-roboto'
 import './popup.css'
@@ -60,42 +60,48 @@ const App: React.FC<{}> = () => {
 
   return (
     <Box mx="8px" my="16px">
-      <Grid container>
-        <Grid item>
-          <Paper>
-            <Box px="15px" py="5px">
-              <InputBase
-                placeholder="Add a city name"
-                value={cityInput}
-                onChange={(event) => {
-                  setCityInput(event.target.value)
-                }}
-              />
-              <IconButton
-                onClick={handleCityButtonClick}
-              >
-                <AddIcon />
-              </IconButton>
-            </Box>
-          </Paper>
+      <Grid container alignItems="flex-end" spacing={2}>
+        <Grid item xs={8}>
+            <form onSubmit={(e) => {
+              handleCityButtonClick();
+              e.preventDefault()
+            }}>
+                <TextField
+                  autoFocus={true}
+                  focused
+                  fullWidth
+                  size="small"
+                  label="City Name"
+                  variant="outlined"
+                  value={cityInput}
+                  onChange={(event) => {
+                    setCityInput(event.target.value)
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          type="submit"
+                          size="small"
+                          color="primary"
+                        >
+                          <AddIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+            </form>
         </Grid>
-        <Grid item>
-          <Paper>
-            <Box py="4px">
-              <IconButton onClick={handleTempScaleButtonClick}>
-                {options.tempScale === 'metric' ? '\u2103' : '\u2109'}
-              </IconButton>
-            </Box>
-          </Paper>
-        </Grid>
-        <Grid item>
-          <Paper>
-            <Box py="4px">
-              <IconButton onClick={handleOverlayButtonClick}>
-                <PictureInPictureIcon />
-              </IconButton>
-            </Box>
-          </Paper>
+        <Grid item xs={4}>
+          <ButtonGroup fullWidth variant="outlined">
+            <Button color="primary" onClick={handleTempScaleButtonClick}>
+              {options.tempScale === 'metric' ? '\u2103' : '\u2109'}
+            </Button>
+            <Button color="primary" onClick={handleOverlayButtonClick}>
+              <PictureInPictureIcon />
+            </Button>
+          </ButtonGroup>
         </Grid>
       </Grid>
       {
